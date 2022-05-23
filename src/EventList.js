9f00e89b77e658f90d849be3e863888838c482e9
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-05-23 11:53:23 +0200

    Writing unit tests for Feature 1, Scenario 1
    Feature1, Scenario1:
    Given the user hasn’t searched for any city,
    When they open the app,
    Then they should see a list of all upcoming events.

---
+++ src/EventList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Event from './Event';
+
+export default function EventList(props){
+    const {events} = props;
+    return(
+        <ul className="EventList">
+            {events.map(event =>
+            <li key={event.id}>
+                <Event
+                       event={event}
+                />
+            </li>
+            )}
+        </ul>
+    )
+}
