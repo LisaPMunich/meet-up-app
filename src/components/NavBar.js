a8f268e6552d13c5682584231d6c88743f2d5afb
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-05-29 22:38:52 +0200

    adjust css styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Icon from '../images/icons8-events-58.png';
+import Icon from '../images/icons8-events-58_yellow.png';
 import '../styling/nav-bar.css'
 
 export default function NavBar() {
     return (
         <nav>
-            <img  src={Icon} alt="event icon" className="nav--icon"/>
-            <h2 className="nav--title">Meet Up App</h2>
+            <img  src={Icon} alt="events icon by Icon8" className="nav--icon"/>
+            <h1 className="nav--title">Meet Up App</h1>
         </nav>
     )
 };
