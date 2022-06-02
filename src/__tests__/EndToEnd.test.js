8ad7a1fe2f23876262811a76410c0c461283a806
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-02 23:25:59 +0200

    Create end-to-end test for Feature "show/hide an event detail"

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,22 +1,40 @@
-import puppeteer from "puppeteer/lib/cjs/puppeteer/node-puppeteer-core";
+import puppeteer from "puppeteer";
 
 
 describe('show/hide an event detail', () => {
-    test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch({
-        // headless: false,
-        // slowMo: 250 // slow down by 250ms
+    beforeEach(async () => {
+        jest.setTimeout(10000)
     });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    await page.waitForSelector('.event');
-
-    const eventDetails = await page.$('.event .event-details');
-    expect(eventDetails).toBeNull();
-    await browser.close();
-});
 
 
-
+    test('An event element is collapsed by default', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+        const eventDetails = await page.$('.event .event__details');
+        expect(eventDetails).toBeNull();
+        await browser.close();
+    });
+    test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+        await page.click('.event .button');
+        const eventDetails = await page.$('.event .event__details');
+        expect(eventDetails).toBeDefined();
+        await browser.close();
+    });
+    test('User can collapse an event to hide its details', async()=> {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+        const eventDetails = await page.$('.event .event__details');
+        expect(eventDetails).toBeDefined();
+        await page.click('.event .button');
+        expect(eventDetails).toBeNull();
+        await browser.close();
+    });
 });
