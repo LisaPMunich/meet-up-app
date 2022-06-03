b1109a49ffd03857033a190068e5c9889db668ef
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-03 12:10:33 +0200

    add await browser.close() to end-to-end testing

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -38,3 +38,43 @@ describe('show/hide an event detail', () => {
         await browser.close();
     });
 });
+
+describe('Filter events by city', () => {
+    beforeEach(async () => {
+        jest.setTimeout(10000)
+    });
+
+    test('When user hasn\'t searched for a city, show upcoming events from all cities.', async()=> {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event-item-wrapper');
+        const eventList = await page.$('.event-item-wrapper');
+        expect(eventList).toBeDefined();
+    });
+
+    test('User should see a list of suggestions when they search for a city', async()=> {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event-item-wrapper');
+        await page.type('.city', 'Berlin, Germany', {delay: 100});
+        const citySuggestions = await page.$('.city-search .suggestions');
+        expect(citySuggestions).toBeDefined();
+    });
+
+    test('User can select a city from the suggested list', async()=> {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const inputSelector = '.city-search .city';
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.city-search');
+        await page.type('.city-search .city', 'Berlin, Germany', {delay: 100});
+        await page.click('.suggestions li:first-of-type');
+        await page.waitForSelector(inputSelector);
+        const value =  await page.$eval(inputSelector, ele => ele.value);
+        expect(value).toContain('Berlin, Germany');
+        await browser.close();
+    });
+
+})
