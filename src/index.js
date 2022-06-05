ecb418359c3967c8d53603b6a0432519e862ad2b
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-05 17:08:34 +0200

    integrate Atatus React Performance Monitoring into Meet App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('6d38d6d1a0d24abb8ebb12b8cbfe9f6d').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
