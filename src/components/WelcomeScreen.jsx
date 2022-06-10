c38eb1b90975e153ce5eafdb239882cc29d7a10e
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-10 23:24:39 +0200

    style WelcomeScreen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,38 +1,45 @@
 import React from "react";
 import '../styling/welcome-screen.css';
+import Icon from '../public/meet-app-512';
+
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
         (
             <div className="WelcomeScreen">
-                <h1>Welcome to the Meet app</h1>
-                <h4>Log in to see upcoming events around the world for
-                    full-stack developers
-                </h4>
-                <div className="button_cont" align="center">
-                    <div class="google-btn">
-                        <div class="google-icon-wrapper">
-                            <img
-                                class="google-icon"
-                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-o.svg"
-                                alt="Google sign-in"
-                            />
+                <div className="WelcomeScreen_content">
+                    <div className="WelcomeScreen_content-text">
+                        <h1>Welcome to the {Icon} <span className="app-name">Meet Up App</span></h1>
+                        <h4>Log in to see upcoming events around the world for
+                            full-stack developers.
+                        </h4>
+                    </div>
+                    <div className="WelcomeScreen_content-buttons">
+                        <div className="button_cont" align="center">
+                            <div class="google-btn">
+                                <div class="google-icon-wrapper">
+                                    <img
+                                        class="google-icon"
+                                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
+        o.svg"
+                                        alt="Google sign-in"
+                                    />
+                                </div>
+                                <button onClick={() => { props.getAccessToken() }}
+                                        rel="nofollow noopener"
+                                        class="btn-text"
+                                >
+                                    <b>Sign in with Google</b>
+                                </button>
+                            </div>
                         </div>
-                        <button onClick={() => { props.getAccessToken() }}
-                                rel="nofollow noopener"
-                                class="btn-text"
+                        <a
+                            href="https://lisapmunich.github.io/meet/privacy.html"
+                            rel="nofollow noopener"
                         >
-                            <b>Sign in with google</b>
-                        </button>
-                        Page 1
+                            Privacy Policy
+                        </a>
                     </div>
                 </div>
-                <a
-                    href="https://lisapmunich.github.io/meet/privacy.html"
-                    rel="nofollow noopener"
-                >
-                    Privacy Policy
-                </a>
             </div>
         )
         : null
