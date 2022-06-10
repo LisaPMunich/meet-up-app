8b51ef37520591ec52c7547c8a3ba00d51936586
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-10 11:24:44 +0200

    correct error in service worker

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import * as serviceWorkerRegistration from '.serviceWorkerRegistration';
+import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-js';
-// atatus.config('6d38d6d1a0d24abb8ebb12b8cbfe9f6d').install();
+import * as atatus from 'atatus-spa';
+atatus.config('6d38d6d1a0d24abb8ebb12b8cbfe9f6d').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +15,7 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
