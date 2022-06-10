45fbd5f0f964fe4f35d67174fc0eeb6466fbbc6a
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-10 12:36:36 +0200

    Performance optimization
    - add height and width to nav image
    - add keys to manifest.json

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './service-worker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-// import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
+import reportWebVitals from "./reportWebVitals";
 atatus.config('6d38d6d1a0d24abb8ebb12b8cbfe9f6d').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,7 +16,4 @@ root.render(
 
 serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
