b5006fb86bb664991ab5342945a30a7f47cd8fa0
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-11 22:32:16 +0200

    style WelcomeScreen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -5,26 +5,26 @@ function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
         (
             <div className="WelcomeScreen">
-                <div className="WelcomeScreen_content">
-                    <div className="WelcomeScreen_content-text">
-                        <h1>Welcome to the <span className="app-name">Meet Up App</span></h1>
-                        <h4>Log in to see upcoming events around the world for
+                <div className="WelcomeScreen__content">
+                    <div>
+                        <h1 className="WelcomeScreen__content--header">Welcome to the <span className="app-name">Meet Up App</span></h1>
+                        <h4 className="WelcomeScreen__content--text">Log in to see upcoming events around the world for
                             full-stack developers.
                         </h4>
                     </div>
-                    <div className="WelcomeScreen_content-buttons">
-                        <div className="button_cont" align="center">
-                            <div class="google-btn">
-                                <div class="google-icon-wrapper">
+                    <div className="WelcomeScreen__content--buttons">
+                        <div align="center">
+                            <div className="google-btn">
+                                <div className="google-icon-wrapper">
                                     <img
-                                        class="google-icon"
+                                        className="google-icon"
                                         src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                                         alt="Google sign-in"
                                     />
                                 </div>
                                 <button onClick={() => { props.getAccessToken() }}
                                         rel="nofollow noopener"
-                                        class="btn-text"
+                                        className="btn-text"
                                 >
                                     <b>Sign in with Google</b>
                                 </button>
@@ -33,6 +33,7 @@ function WelcomeScreen(props) {
                         <a
                             href="https://lisapmunich.github.io/meet/privacy.html"
                             rel="nofollow noopener"
+                            className="privacy-policy-link"
                         >
                             Privacy Policy
                         </a>
