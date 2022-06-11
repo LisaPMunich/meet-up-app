9b7537cba5a04a4d55059316853597e6ace93933
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-11 23:00:16 +0200

    solve blur of nav bar icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import '../styling/nav-bar.css'
 export default function NavBar() {
     return (
         <nav>
-            <img  src={Icon} width={58} height={58} alt="events icon by Icon8" className="nav--icon"/>
+            <img  src={Icon} alt="events icon by Icon8" className="nav--icon"/>
             <h1 className="nav--title">Meet Up App</h1>
         </nav>
     )
