30f90555068fe972fac886e928a39b30322eb4df
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-13 11:24:57 +0200

    Update href on WelcomeScreen to heroku

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -31,7 +31,7 @@ function WelcomeScreen(props) {
                             </div>
                         </div>
                         <a
-                            href="https://lisapmunich.github.io/meet/privacy.html"
+                            href="https://LisaPMunich.github.io/meet/privacy.html"
                             rel="nofollow noopener"
                             className="privacy-policy-link"
                         >
