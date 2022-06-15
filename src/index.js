6829b2ff79e930ace73f3f7a3f60f3e87951901e
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-06-15 09:31:49 +0200

    Add Scatter chart with mock-data

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
 import reportWebVitals from "./reportWebVitals";
 //atatus.config('6d38d6d1a0d24abb8ebb12b8cbfe9f6d').install();
 
